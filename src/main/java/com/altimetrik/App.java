ee7ef7ec952c1a68abdc9d62af05217612f4609b
Ravindra  Chacravarti <ravindra1986@users.noreply.github.com>
2020-07-20 11:38:28 -0700

    Add files via upload

--- src/main/java/com/altimetrik/App.java
+++ src/main/java/com/altimetrik/App.java
@@ -1,13 +1,20 @@
 package com.altimetrik;
 
-/**
- * Hello world!
- *
- */
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
